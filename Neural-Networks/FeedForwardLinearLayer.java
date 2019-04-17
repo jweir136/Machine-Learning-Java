98429006178664a2e379d8e9baf17379a86acfd7
jweir136 <31812897+jweir136@users.noreply.github.com>
2019-04-17 12:37:06 -0400

    Add FeedForwardLinearLayer.java
    This is a very simple Linear ANN that only takes one dimensional data. It uses the feed forward method to optimize the single weight. There is no activation function.

---
+++ Neural-Networks/FeedForwardLinearLayer.java
@@ -0,0 +1,71 @@
+public class FeedForwardLinearLayer extends Model {
+  private double[][] X;
+  private double[] y;
+  private double learningRate, weight;
+  private int maxEpochs;
+
+  public FeedForwardLinearLayer(double learningRate, int maxEpochs) {
+    this.learningRate = learningRate;
+    this.maxEpochs = maxEpochs;
+    this.weight = Math.random();
+  }
+
+  public void fit(double[][] X, double[] y) {
+    this.X = X;
+    this.y = y;
+  }
+
+  public void train() {
+    double[] pastWeights = new double[this.maxEpochs];
+    pastWeights[0] = this.weight;
+
+    for (int i = 0; i < this.maxEpochs; i++) {
+      this.weight -= this.learningRate * deriv();
+
+      if ((this.weight - pastWeights[pastWeights.length-2]) < 1e-3) {
+        System.out.println("[+]\tConverged at epoch=" + i);
+        break;
+      }
+
+      pastWeights[i+1] = this.weight;
+
+      System.out.println("[!]\tEpoch=" + i + "\tMSE=" + error(this.X, this.y));
+    }
+  }
+
+  public double[] predict(double[][] X) {
+    double[] predictions = new double[X.length];
+
+    for (int i = 0; i < X.length; i++) {
+      predictions[i] = this.weight * X[i][0];
+    }
+
+    return predictions;
+  }
+
+  public double error(double[][] X, double[] y) {
+    double[] predictions = predict(X);
+    double error = 0.0;
+
+    for (int i = 0; i < X.length; i++) {
+      error += Math.pow(y[i] - predictions[i], 2);
+    }
+
+    return error / X.length;
+  }
+
+  public void close() {
+    this.weight = Math.random();
+  }
+
+  private double deriv() {
+    double[] predictions = predict(this.X);
+    double error = 0.0;
+
+    for (int i = 0; i < this.X.length; i++) {
+      error -= this.y[i] - predictions[i];
+    }
+
+    return (2.0 * error) / this.y.length;
+  }
+}
